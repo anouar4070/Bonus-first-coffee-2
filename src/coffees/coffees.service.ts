6cfda73a99caa01c9856533bb8d836073c9711b4
anouar garbaa <g.anouar@yahoo.com>
2025-07-20 18:49:48 +0200

    Using the Mongoose Model to Access MongoDB

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,46 +1,80 @@
+import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
 
 @Injectable()
 export class CoffeesService {
-  private coffees: Coffee[] = [
-    {
-      id: 1,
-      name: 'Anouar 10',
-      brand: 'Adidas',
-      flavors: ['chocolate', 'vanilla'],
-    },
-  ];
+  // private coffees: Coffee[] = [
+  //   {
+  //     id: 1,
+  //     name: 'Anouar 10',
+  //     brand: 'Adidas',
+  //     flavors: ['chocolate', 'vanilla'],
+  //   },
+  // ];
+
+  constructor(
+    @InjectModel(Coffee.name) private readonly coffeeModel: Model<Coffee>,
+  ) {}
 
   findAll() {
-    return this.coffees;
+    //return this.coffees;
+    return this.coffeeModel.find().exec();
   }
 
-  findOne(id: string) {
-    const coffee = this.coffees.find((item) => item.id === +id);
+  // findOne(id: string) {
+  //   const coffee = this.coffees.find((item) => item.id === +id);
+  //   if (!coffee) {
+  //     //throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
+  //     throw new NotFoundException(`Coffee #${id} not found`);
+  //   }
+  //   return coffee;
+  // }
+  async findOne(id: string) {
+    const coffee = await this.coffeeModel.findOne({ _id: id }).exec();
     if (!coffee) {
-      //throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
       throw new NotFoundException(`Coffee #${id} not found`);
     }
     return coffee;
   }
 
-  create(createCoffeeDto: any) {
-    this.coffees.push(createCoffeeDto);
-    return createCoffeeDto;
+  // create(createCoffeeDto: any) {
+  //   this.coffees.push(createCoffeeDto);
+  //   return createCoffeeDto;
+  // }
+  create(createCoffeeDto: CreateCoffeeDto) {
+    const coffee = new this.coffeeModel(createCoffeeDto);
+    return coffee.save();
   }
 
-  update(id: string, updateCoffDto: any) {
-    const existingCoffee = this.findOne(id);
-    if (existingCoffee) {
-      //update here
+  // update(id: string, updateCoffDto: any) {
+  //   const existingCoffee = this.findOne(id);
+  //   if (existingCoffee) {
+  //     //update here
+  //   }
+  // }
+  async update(id: string, updateCoffDto: UpdateCoffeeDto) {
+    const existingCoffee = await this.coffeeModel
+      .findOneAndUpdate({ _id: id }, { $set: updateCoffDto }, { new: true })
+      .exec();
+    if (!existingCoffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
     }
+    return existingCoffee;
   }
 
-  remove(id: string) {
-    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
-    if (coffeeIndex >= 0) {
-      this.coffees.splice(coffeeIndex, 1);
-    }
+  // remove(id: string) {
+  //   const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+  //   if (coffeeIndex >= 0) {
+  //     this.coffees.splice(coffeeIndex, 1);
+  //   }
+  // }
+  async remove(id: string) {
+    const coffee = await this.findOne(id);
+    //return coffee.remove(); //In Mongoose v6+, remove() has been deprecated
+    return coffee.deleteOne();
   }
 }
